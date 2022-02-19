3a44b243a603771bf7bb798a9ce2e3d4d82c9190
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-02-18 21:15:53 -0600

    fix: fake clear inventory now works correctly

--- src/main/java/com/github/diegonighty/wordle/utils/FakeInventory.java
+++ src/main/java/com/github/diegonighty/wordle/utils/FakeInventory.java
@@ -8,23 +8,14 @@ import org.bukkit.inventory.ItemStack;
 public class FakeInventory {
 
 	private static final ItemStack AIR = new ItemStack(Material.AIR);
+	private static final int INVENTORY_SIZE = 46;
 
 	public static void clearInventoryFor(PacketHandler packetHandler, Player player) {
 		byte window = -2;
-		int slot = 9;
 
-		for (int i = 0; i < player.getInventory().getContents().length; i++) {
-			packetHandler.setFakeItem(player, window, slot++, AIR);
+		for (int i = 0; i < INVENTORY_SIZE; i++) {
+			packetHandler.setFakeItem(player, window, i, AIR);
 		}
-	}
-
-	public static void setFakeItem(PacketHandler packetHandler, Player player, int slot, ItemStack item) {
-		packetHandler.setFakeItem(
-				player,
-				(byte) packetHandler.currentWindowID(player),
-				slot,
-				item
-		);
 	}
 
 
